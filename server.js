c6d0dc910fff7c686cda25267763622135d93e00
nonm23911-cell <nonm23911@gmail.com>
2026-08-25 14:56:15 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+app.get('/', (req, res) => {
+  res.send('سيرفر الدردشة يعمل بنجاح! 🚀');
+});
+
+io.on('connection', (socket) => {
+  socket.on('send_message', (data) => {
+    io.emit('receive_message', data);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
